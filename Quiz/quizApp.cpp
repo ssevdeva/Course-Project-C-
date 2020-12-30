f98ae9cfd76ba67285271276cd6a44043d4d6ffa
ssevdeva <72700165+ssevdeva@users.noreply.github.com>
2020-12-30 20:32:10 +0200

    Project basis

---
+++ Quiz/quizApp.cpp
@@ -0,0 +1,21 @@
+/**
+*
+* Solution to course project # 1
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2020/2021
+*
+* @author Sanya Sevdeva
+* @idnumber 62517
+* @compiler GCC
+*
+* <INSERT DESCRIPTION HERE>
+*
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    return 0;
+}
---
+++ Quiz/quizFunc.cpp
@@ -0,0 +1,15 @@
+/**
+*
+* Solution to course project # 1
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2020/2021
+*
+* @author Sanya Sevdeva
+* @idnumber 62517
+* @compiler GCC
+*
+* <file with helper functions>
+*
+*/
+
